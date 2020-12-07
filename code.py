8c1ca0a868fa89cd20502387e979088dd483fec0
Jonathan Silverman <jsilverman2@gmail.com>
2020-12-06 19:44:36 -0700

    vampire confusing ESP multi sensor

--- code.py
+++ code.py
@@ -35,9 +35,24 @@ samples = array.array('H', [0] * 160)
 mic.record(samples, len(samples))
 
 while True:
+    time.sleep(0.1)
     mic.record(samples, len(samples))
     magnitude = normalized_rms(samples)
-    print(((magnitude),))
-    time.sleep(0.1)
-    cp.pixels[0] = (0, int(magnitude), 0)
-    cp.pixels[1] = (0, 0, int(cp.light))
+    cp.pixels[0] = (0, int(magnitude), 0) # sound sensor
+    cp.pixels[1] = (0, 0, int(cp.light)) # light sensor
+    cp.pixels[2] = (int(cp.temperature), 0, 0)
+    if cp.shake(shake_threshold=20):
+        print("Shake detected!")
+        cp.red_led = True
+        cp.pixels[3] = (255, 255, 0)
+        time.sleep(15)
+    else:
+        cp.red_led = False
+        cp.pixels[3] = (0, 0, 0)
+    x, y, z = cp.acceleration
+    cp.pixels[4] = (int(x), int(y), int(z))
+    cp.pixels[5] = (int(magnitude), int(cp.light), int(cp.temperature))
+    cp.pixels[6] = (31, 31, 31)
+    cp.pixels[7] = (63, 63, 63)
+    cp.pixels[8] = (127, 127, 127)
+    cp.pixels[9] = (255, 255, 255)
